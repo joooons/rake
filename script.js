fec123808644ace38b0c4847a186a32f1b12780a
joooons <52220710+joooons@users.noreply.github.com>
2024-02-22 15:53:33 -0800

    mod README.md; simply change textContent for 1 h1

---
+++ script.js
@@ -0,0 +1,8 @@
+let img = 'https://bit.ly/4bHcA0l'
+
+console.log('---------------- ok then ----------------------')
+
+const elem = document.getElementsByTagName('h1')
+
+elem[0].textContent = 'NOPE'
+
