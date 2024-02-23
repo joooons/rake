07930f49acdba905aecf1a0e56839324f119f2bb
joooons <52220710+joooons@users.noreply.github.com>
2024-02-23 00:02:27 -0800

    add url and message to popup

--- popup/popup.js
+++ popup/popup.js
@@ -1,4 +1,8 @@
 const button = document.getElementById('button')
+const url = document.getElementById('url')
+const message = document.getElementById('message')
+
+const linkedinRe = /linkedin\.com\/jobs\/view/;
 
 async function getCurrentTab() {
     let queryOptions = { active: true, lastFocusedWindow: true };
@@ -6,14 +10,24 @@ async function getCurrentTab() {
     return tab;
 }
 
+console.log('chrome extention Rake loaded')
+
 button.addEventListener('click', async function () {
     const currentTab = await getCurrentTab();
+
     if (currentTab) {
-        chrome.scripting.executeScript({
-            target: { tabId: currentTab.id },
-            files: ['./scripts/content.js']
-        }).then(() => {
-            console.log('script executed in the current tab');
-        });
+        url.textContent = currentTab.url
+
+        if (linkedinRe.test(currentTab.url)) {
+            message.textContent = 'attempting download...'
+            chrome.scripting.executeScript({
+                target: { tabId: currentTab.id },
+                files: ['./scripts/content.js']
+            }).then(() => {
+                console.log('script executed in the current tab');
+            });
+        } else {
+            message.textContent = 'not supported'
+        }
     }
 });
