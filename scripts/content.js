bd4f4166cddf215593d09b0ebd48dd5ca0cd830c
joooons <52220710+joooons@users.noreply.github.com>
2024-02-22 17:58:19 -0800

    saving file with preset text from google.com works

--- scripts/content.js
+++ scripts/content.js
@@ -1,6 +1,5 @@
-// let img = 'https://bit.ly/4bHcA0l'
-
 const re = /(notnewspage)/;
+const other = /(google)/;
 const url = window.document.URL
 
 if (re.test(url)) {
@@ -10,6 +9,20 @@ if (re.test(url)) {
         para[0].textContent = 'NOPE'
         console.log(para[0].textContent)
     }
+} else if (other.test(url)) {
+    console.log('Rake works on', url);
+
+    const text = "testing only"
+    const fileName = "raked.txt"
+    const blob = new Blob([text], { type: 'text/plain;charset=utf-8' });
+
+    const link = document.createElement('a');
+    link.href = URL.createObjectURL(blob);
+    link.download = fileName;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+
 } else {
     console.log('Rake does not work on', url);
 }
