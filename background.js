9ddbafa8dc5bb31d9907a10d6d7cf85a3a73cefe
joooons <52220710+joooons@users.noreply.github.com>
2024-02-22 17:38:27 -0800

    popup click works on active tab content

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 chrome.runtime.onInstalled.addListener(() => {
-    console.log('hi')
+    console.log('from background.js')
     chrome.action.setBadgeText({
         text: "OFF",
     });
@@ -9,6 +9,7 @@ const extensions = 'https://developer.chrome.com/docs/extensions'
 const webstore = 'https://developer.chrome.com/docs/webstore'
 
 chrome.action.onClicked.addListener(async (tab) => {
+    console.log('did click work?')
     if (tab.url.startsWith(extensions) || tab.url.startsWith(webstore)) {
         // Retrieve the action badge to check if the extension is 'ON' or 'OFF'
         const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
--- popup/popup.js
+++ popup/popup.js
@@ -1,9 +1,19 @@
 const button = document.getElementById('button')
 
-button.addEventListener('click', function () {
-    alert('poppy at least does this')
-    // chrome.scripting.executeScript({
-    //     target: { tabId: getTabId() },
-    //     files: ['script.js']
-    // }).then(() => { console.log('script works') })
-})
+async function getCurrentTab() {
+    let queryOptions = { active: true, lastFocusedWindow: true };
+    let [tab] = await chrome.tabs.query(queryOptions);
+    return tab;
+}
+
+button.addEventListener('click', async function () {
+    const currentTab = await getCurrentTab();
+    if (currentTab) {
+        chrome.scripting.executeScript({
+            target: { tabId: currentTab.id },
+            files: ['./scripts/content.js']
+        }).then(() => {
+            console.log('script executed in the current tab');
+        });
+    }
+});
