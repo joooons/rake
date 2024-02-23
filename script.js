2a765e01097e126d6c6fa4ef6a4883eb16472624
joooons <52220710+joooons@users.noreply.github.com>
2024-02-22 16:04:44 -0800

    @manifest.json multiple matches work

--- script.js
+++ script.js
@@ -3,6 +3,17 @@ let img = 'https://bit.ly/4bHcA0l'
 console.log('---------------- ok then ----------------------')
 
 const elem = document.getElementsByTagName('h1')
+const para = document.getElementsByClassName('br-article-title')
 
-elem[0].textContent = 'NOPE'
+// if (elem) {
+//     elem[0].textContent = 'NOPE'
+// }
+
+if (para) {
+    para[0].textContent = 'NOPE'
+}
+
+para[0].textContent = 'NOPE'
+
+console.log(para[0].textContent)
 
